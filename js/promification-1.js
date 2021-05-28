fbcf6ab1350ae26f43625e99dd26e81d6c5bbfd6
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-05-28 10:06:43 +0300

    Update promification-1.js

--- js/promification-1.js
+++ js/promification-1.js
@@ -3,7 +3,11 @@
 // миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-    // Твой код
+    return new Promise(resolve => {
+     setTimeout(() => {
+         resolve(ms)
+     }, ms);
+ })
   };
   
   const logger = time => console.log(`Resolved after ${time}ms`);
